14a4c44d32d87e800b10032b08809e6f4ccce5fb
khashashin <khashashin@gmail.com>
2018-02-24 02:48:10 +0100

    added intro

--- home/models.py
+++ home/models.py
@@ -2,8 +2,14 @@ from __future__ import absolute_import, unicode_literals
 
 from django.db import models
 
+from wagtail.wagtailcore.fields import RichTextField
+from wagtail.wagtailadmin.edit_handlers import FieldPanel
 from wagtail.wagtailcore.models import Page
 
 
 class HomePage(Page):
-    pass
+    intro = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('intro')
+    ]
